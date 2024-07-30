7f61ad52b15b75f956eff17a46708a70294d14d0
kubraacelik <kkbra.celik92@gmail.com>
2024-07-30 11:09:25 +0300

    message model class was created

---
+++ RabbitMQ-EMailQueue/MessageModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQ_EMailQueue
+{
+    public class MessageModel
+    {
+        public string mail {  get; set; }
+        public string message { get; set; } = "Rabbitmq Dersi";
+        public string subject { get; set; } = "Rabbitmq 2024";
+    }
+}
